cfd425047871813b3cae94b6f83f7514407d75f2
Paulo Cezar Rodrigues da Silva <conhecer.ti@gmail.com>
2024-04-10 16:33:40 -0400

    Projeto criado

---
+++ src/test/java/com/pcrs/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcrs.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
