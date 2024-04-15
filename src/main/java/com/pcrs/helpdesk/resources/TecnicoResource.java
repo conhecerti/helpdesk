39c701b5e92efe1f77c306af007c9bb56a92afc8
Paulo Cezar Rodrigues da Silva <conhecer.ti@gmail.com>
2024-04-15 18:58:33 -0400

    endpoint findById para Tecnico

--- src/main/java/com/pcrs/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/pcrs/helpdesk/resources/TecnicoResource.java
@@ -1,2 +1,24 @@
-package com.pcrs.helpdesk.resources;public class TecnicoResource {
+package com.pcrs.helpdesk.resources;
+
+import com.pcrs.helpdesk.domain.Tecnico;
+import com.pcrs.helpdesk.services.TecnicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tecnicos")
+public class TecnicoResource {
+
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+        Tecnico tecnico = tecnicoService.findById(id);
+        return ResponseEntity.ok().body(tecnico);
+    }
 }
--- src/main/java/com/pcrs/helpdesk/services/TecnicoService.java
+++ src/main/java/com/pcrs/helpdesk/services/TecnicoService.java
@@ -1,2 +1,22 @@
-package com.pcrs.helpdesk.services;public class TecnicoService {
+package com.pcrs.helpdesk.services;
+
+import com.pcrs.helpdesk.domain.Tecnico;
+import com.pcrs.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
+
 }
