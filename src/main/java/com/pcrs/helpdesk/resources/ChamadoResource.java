a867934238915e69ca4b6f3fc4d36f5cede504c9
Paulo Cezar Rodrigues da Silva <conhecer.ti@gmail.com>
2024-04-23 15:59:05 -0400

    findAll para Chamado

--- src/main/java/com/pcrs/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/pcrs/helpdesk/resources/ChamadoResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/chamados")
 public class ChamadoResource {
@@ -23,5 +26,11 @@ public class ChamadoResource {
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
     }
 
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> list = chamadoService.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
 
 }
--- src/main/java/com/pcrs/helpdesk/services/ChamadoService.java
+++ src/main/java/com/pcrs/helpdesk/services/ChamadoService.java
@@ -7,6 +7,7 @@ import com.pcrs.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,9 @@ public class ChamadoService {
         Optional<Chamado> obj = chamadoRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id ));
 
+    }
+
+    public List<Chamado> findAll() {
+        return chamadoRepository.findAll();
     }
 }
