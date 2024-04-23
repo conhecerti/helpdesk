b95d792441baa56421325c01c0c710870eeca54e
Paulo Cezar Rodrigues da Silva <conhecer.ti@gmail.com>
2024-04-23 15:50:01 -0400

    findById para chamado

---
+++ src/main/java/com/pcrs/helpdesk/services/ChamadoService.java
@@ -0,0 +1,23 @@
+package com.pcrs.helpdesk.services;
+
+import com.pcrs.helpdesk.domain.Chamado;
+import com.pcrs.helpdesk.repositories.ChamadoRepository;
+import com.pcrs.helpdesk.resources.ChamadoResource;
+import com.pcrs.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = chamadoRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id ));
+
+    }
+}
